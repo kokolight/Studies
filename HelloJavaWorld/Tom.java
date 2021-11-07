3ca17ebd7c86b3e211c736c085f6c4b609eba499
Tom Segal <tom@axissecurity.com>
2021-11-07 19:31:26 +0200

    Changed filed to Tom.java

--- HelloJavaWorld/Tom.java
+++ HelloJavaWorld/Tom.java
@@ -1,7 +1,7 @@
-public class Hello{
+public class Tom{
   public static void main(String[] args){
     for(int i=0; i<10; i++){
-      System.out.println("Hello Devops After Push..."+i);
+      System.out.println("Hello From Tom!!!..."+i);
     }
   }
 }
